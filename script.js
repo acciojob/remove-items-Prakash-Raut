0ecd24e354a98110df8363f0868a66eb80c58b62
Prakash Raut <prakash_raut@outlook.com>
2025-08-14 16:34:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const select = document.getElementById('colorSelect');
+const btn = document.querySelector('input[type="button"]');
+
+btn.addEventListener('click', () => {
+  const selectedIndex = select.selectedIndex;
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+});
